a7fe0c1ae85478bc698dc7486e8b2d5c88f95929
SamylleRose <samylle1brito@gmail.com>
2025-07-01 20:31:42 -0300

    inition commit: configured the fire base system.

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:authentication_flutter/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => AuthService())],
+      child: MyApplication(),
+    ),
+  );
+}
